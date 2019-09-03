691faa92ca2549f26c6993c310b4c128ad73bb51
Andres <andresc@rumbleon.com>
2019-09-03 18:26:03 -0500

    erased default appjs content, created images file

---
+++ src/config/images.js
@@ -0,0 +1,21 @@
+const images = {
+  artClear: require('../assets/art_clear.png'),
+  artClouds: require('../assets/art_clouds.png'),
+  artFog: require('../assets/art_fog.png'),
+  artLightClouds: require('../assets/art_light_clouds'),
+  artLightRain: require('../assets/art_light_rain.png'),
+  artRain: require('../assets/art_rain.png'),
+  artSnow: require('../assets/art_snow.png'),
+  artStorm: require('../assets/art_storm.png'),
+  icClear: require('../assets/ic_clear.png'),
+  icCloudy: require('../assets/ic_cloudy.png'),
+  icFog: require('../assets/ic_fog.png'),
+  icLightClouds: require('../assets/ic_light_clouds.png'),
+  icLightRain: require('../assets/ic_light_rain.png'),
+  icLogo: require('../assets/ic_logo.png'),
+  icRain: require('../assets/ic_rain.png'),
+  icSnow: require('../assets/ic_snow.png'),
+  icStorm: require('../assets/ic_storm.png'),
+};
+
+export default images;
