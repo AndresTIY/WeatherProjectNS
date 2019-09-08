9f08fa51eb9586082b1f84f1d5cedc2b2b7807a7
Andres <andresc@rumbleon.com>
2019-09-08 12:31:09 -0500

    changed api, showing results based on if i % 8

--- src/api/api.js
+++ src/api/api.js
@@ -6,6 +6,10 @@ const city = 'q=dallas,us';
 
 const currentWeatherAPI = `${url}weather?${city}&units=imperial&${appID}`;
 
-const fiveDayForecastAPI = `${url}forecast?${city}&units=imperial&cnt=5&${appID}`;
+// const fiveDayForecastAPI = `${url}forecast?${city}&units=imperial${appID}`;
+const fiveDayForecastAPI = `${url}forecast?${city}&units=imperial&${appID}`;
+// const fiveDayForecastAPI = `${url}forecast?${city}&units=imperial&cnt=5&${appID}`;
 
 export {currentWeatherAPI, fiveDayForecastAPI};
+
+// http://api.openweathermap.org/data/2.5/forecast?q=dallas,us&units=imperial&appid=d6c0f3de6a27ad3154235bf91005e061
